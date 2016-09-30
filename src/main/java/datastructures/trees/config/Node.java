2096e13fe781d89da80458c0a8471446c3828a3d
dszopa <szopanator@gmail.com>
2016-09-30 08:05:49 -0500

    Add Node class to be used by future trees

---
+++ src/main/java/datastructures/trees/config/Node.java
@@ -0,0 +1,13 @@
+package datastructures.trees.config;
+
+public class Node {
+
+    public Node right, left;
+    public int value;
+
+    public Node() {
+        left = null;
+        right = null;
+        value = 0;
+    }
+}
